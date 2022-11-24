a1b021ae181c0124c560b408c5562987c4d75127
Andrea Perin <andrea.perin@irz.uni-hannover.de>
2022-11-24 10:47:52 +0100

    Example update

--- demo/continuousnodeprova.jl
+++ demo/continuousnodeprova.jl
@@ -16,19 +16,22 @@ grandparent = StdNode(CPDc)
 
 
 parents_dispersivitivy_longv = [timescenario, grandparent]
-dispersivitivy_longv1 = truncated(Normal(1, 1), lower=0)
-dispersivitivy_longv2 = truncated(Normal(2, 1), lower=0)
-dispersivitivy_longv3 = truncated(Normal(2, 2), lower=0.1)
-dispersivitivy_longv4 = truncated(Normal(10, 1), lower=0)
-dispersivitivy_longv5 = truncated(Normal(20, 1), lower=0)
-dispersivitivy_longv6 = truncated(Normal(22, 2), lower=0.1)
-dispersivitivy_longv7 = truncated(Normal(22, 2), lower=0.1)
-dispersivitivy_longv8 = truncated(Normal(22, 2), lower=0.1)
-dispersivitivy_longv9 = truncated(Normal(22, 2), lower=0.1)
-
+dispersivitivy_longv1 = NamedCategorical([:disp1, :disp2], [0.9, 0.1])
+dispersivitivy_longv2 = NamedCategorical([:disp1, :disp2], [0.8, 0.2])
+dispersivitivy_longv3 = NamedCategorical([:disp1, :disp2], [0.7, 0.3])
+dispersivitivy_longv4 = NamedCategorical([:disp1, :disp2], [0.6, 0.4])
+dispersivitivy_longv5 = NamedCategorical([:disp1, :disp2], [0.5, 0.5])
+dispersivitivy_longv6 = NamedCategorical([:disp1, :disp2], [0.4, 0.6])
+dispersivitivy_longv7 = NamedCategorical([:disp1, :disp2], [0.3, 0.7])
+dispersivitivy_longv8 = NamedCategorical([:disp1, :disp2], [0.2, 0.8])
+dispersivitivy_longv9 = NamedCategorical([:disp1, :disp2], [0.1, 0.9])
 dist = [dispersivitivy_longv1, dispersivitivy_longv2, dispersivitivy_longv3, dispersivitivy_longv4, dispersivitivy_longv5, dispersivitivy_longv6, dispersivitivy_longv7, dispersivitivy_longv8, dispersivitivy_longv2]
-CPDd = CategoricalCPD(:flow, [:time_scenario, :grandparent], [3, 3], dist)
-dispersivitivy_longv = StdNode(CPDd, parents_dispersivitivy_longv)
+CPDd = CategoricalCPD(:dispersivity, [:time_scenario, :grandparent], [3, 3], dist)
+model_input_disp = Dict(
+    :disp1 => (:disp_longv, 2.0, FormatSpec(".8e")),
+    :disp2 => (:disp_longv, 9.0, FormatSpec(".8e"))
+)
+dispersivitivy_longv = StdNode(CPDd, parents_dispersivitivy_longv, model_input_disp)
 
 
 parents_Kz = [timescenario, grandparent, prova]
@@ -47,30 +50,18 @@ Kz2_3_2 = truncated(Normal(1, 1), lower=0)
 Kz3_1_1 = truncated(Normal(1, 1), lower=0)
 Kz3_1_2 = truncated(Normal(1, 1), lower=0)
 Kz3_2_1 = truncated(Normal(1, 1), lower=0)
+Kz3_2_2 = truncated(Normal(1, 1), lower=0)
 Kz3_3_1 = truncated(Normal(1, 1), lower=0)
-Kz3_3_2 = truncated(Normal(1, 1), lower=0)
-Kz_cpd = [
-    Kz1_1_1,
-    Kz1_1_2,
-    Kz1_2_1,
-    Kz1_2_2,
-    Kz1_3_1,
-    Kz1_3_2,
-    Kz2_1_1,
-    Kz2_1_2,
-    Kz2_2_1,
-    Kz2_2_2,
-    kz2_3_1,
-    Kz2_3_2,
-    Kz3_1_1,
-    Kz3_1_2,
-    Kz3_2_1,
-    Kz3_3_1,
-    Kz3_3_2
-]
+Kz3_3_2 = truncated(Normal(4, 2), lower=0.1)
+Kz_cpd = [Kz1_1_1, Kz1_1_2, Kz1_2_1, Kz1_2_2, Kz1_3_1, Kz1_3_2, Kz2_1_1, Kz2_1_2, Kz2_2_1, Kz2_2_2, kz2_3_1, Kz2_3_2, Kz3_1_1, Kz3_1_2, Kz3_2_1, Kz3_2_2, Kz3_3_1, Kz3_3_2]
+CPDKz = CategoricalCPD(:Kz, name.(parents_Kz), [3, 3, 2], Kz_cpd)
+model_input_Kz = [(:K_z, FormatSpec(".8e"))]
+Kz = StdNode(CPDKz, parents_Kz, model_input_Kz)
 
-CPDKz = CategoricalCPD{Distribution}(:Kz, [:time_scenario, :grandparent], [3, 3, 2], Kz_cpd)
-Kz = StdNode(CPDKz, parents_Kz)
+bn = StdBayesNet([timescenario, grandparent, prova, Kz, dispersivitivy_longv])
+show(bn)
+
+
 
 parents_th = [dispersivitivy_longv, Kz]
 sim_th = MonteCarlo(2)
@@ -82,4 +73,8 @@ extras = String[]
 solvername = "smokerV3TC"
 output_parameters = xlsx2output_parameter(default_file)
 performances = build_performances(output_parameters)
-th_node = ModelNode(:th_node, parents_th, default_inputs_th, sourcedir, source_file, extras, solvername, output_parameters, performances, true, sim_th)
+
+
+
+
+# th_node = ModelNode(:th_node, parents_th, default_inputs_th, sourcedir, source_file, extras, solvername, output_parameters, performances, true, sim_th)
