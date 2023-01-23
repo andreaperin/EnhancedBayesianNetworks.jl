416e2d73419226d84c4a849dbe81dc6fa55ddee6
andrea perin <andrea.perin@irz.uni-hannover.de>
2023-01-23 17:04:36 +0100

    partial addition of ModelInputNodes, whose struct is still a WIP cause of the cpd with evidence evaluation

--- demo/prova_parets.jl
+++ demo/prova_parets.jl
@@ -7,7 +7,12 @@ include("../bn.jl")
 
 a = NamedCategorical([:first, :second, :third], [0.34, 0.33, 0.33])
 CPDa = RootCPD(:t, a)
-t = StdNode(CPDa)
+map2uq_a = Dict{Symbol,UQInput}(
+    :first => Parameter(1.1, :time),
+    :second => Parameter(2.2, :time),
+    :third => Parameter(3.3, :time)
+)
+t = ModelInputNode(CPDa, map2uq_a)
 
 b = NamedCategorical([:happen, :nothappen], [0.5, 0.5])
 CPDp = RootCPD(:p, b)
@@ -29,7 +34,13 @@ duration1 = NamedCategorical([:day1, :day10, :day100], [1.0, 0.0, 0.0])
 duration2 = NamedCategorical([:day1, :day10, :day100], [0.0, 1.0, 0.0])
 duration3 = NamedCategorical([:day1, :day10, :day100], [0.0, 0.0, 1.0])
 CPDduration = CategoricalCPD(:k, name.(parents_simduration), [3], [duration1, duration2, duration3])
-k = StdNode(CPDduration, parents_simduration)
+map2uq_k = Dict{Symbol,UQInput}(
+    :day1 => Parameter(1, :days),
+    :day10 => Parameter(10, :days),
+    :day100 => Parameter(100, :days)
+)
+k = ModelInputNode(CPDduration, parents_simduration, map2uq_k)
+
 
 wraising1 = Rayleigh(0.5)
 wraising2 = Rayleigh(2.5)
@@ -38,4 +49,24 @@ parents_m = [t]
 CPD_m = CategoricalCPD(:m, name.(parents_m), [3], [wraising1, wraising2, wraising3])
 m = StdNode(CPD_m, parents_m)
 
-par = [t, k, m, w, h]
+par = [t, k, m]
+
+discrete, cont_nonroot, cont_root = nodes_split(par)
+append!(discrete, cont_root)
+while ~isempty(cont_nonroot)
+    new_parents = Vector{AbstractNode}()
+    for single_cont_nonroot in cont_nonroot
+        append!(new_parents, single_cont_nonroot.parents)
+    end
+    discrete_new, cont_nonroot_new, cont_root_new = nodes_split(new_parents)
+    append!(discrete, discrete_new)
+    append!(discrete, cont_root_new)
+    cont_nonroot = cont_nonroot_new
+end
+ancestors = unique(discrete)
+states_combinations = get_combinations(ancestors)
+
+## TODO for each state_comb, valutare quali node cont_nonroot mancano ad ancestors rispetto a parents, valutarli data la state combination e aggiungere la distribuzione!
+evidence = Assignment(:windvelocity => :fast, :emission => :happen)
+a = evaluate_nodecpd_with_evidence(bn, name(node_waveraising), evidence)
+## evaluate_nodecpd_with_evidence requires BN, maybe the function should be changed to just pass the parents(?)
--- nodes.jl
+++ nodes.jl
@@ -15,7 +15,7 @@ struct ModelNode <: Node
     parents::Vector{T} where {T<:AbstractNode}
     type::String
     ## TODO check if this function works
-    function ModelNode(target::NodeName, parents::Vector{T}, model::Vector{UQModel}, typpe::String, type::String) where {T<:AbstractNode}
+    function ModelNode(target::NodeName, parents::Vector{T}, model::Vector{UQModel}, type::String) where {T<:AbstractNode}
         discrete, cont_nonroot, cont_root = nodes_split(par)
         append!(discrete, cont_root)
         while ~isempty(cont_nonroot)
@@ -29,6 +29,7 @@ struct ModelNode <: Node
             cont_nonroot = cont_nonroot_new
         end
         ancestors = unique(discrete)
+        states_combinations = get_combinations(ancestors)
 
     end
 end
@@ -84,19 +85,71 @@ struct StdNode <: Node
 end
 
 
-#####
+struct ModelInputNode <: AbstractNode
+    cpd::CPD
+    parents::Vector{T} where {T<:AbstractNode}
+    type::String
+    map2uqinputs::Dict{Symbol,UQInput}
+    ##TODO add to log
+    function ModelInputNode(cpd::CPD, parents::Vector{T}, type::String, map2uqinputs::Dict{Symbol,UQInput}) where {T<:AbstractNode}
+        node_name = cpd.target
+        if ~isa(cpd, RootCPD)
+            cpd.parents == name.(parents) ? new(cpd, parents, type, map2uqinputs) : throw(DomainError(node_name, "Assigned parents are not equals to the one of CPD"))
+            length(filter(x -> x.type == "discrete", parents)) == length(cpd.parental_ncategories) ? new(cpd, parents, type, map2uqinputs) : throw(DomainError(node_name, "parents mismatch in CPD for discrete parents and parental_ncategories in $node_name"))
+        else
+            new(cpd, parents, type, map2uqinputs)
+        end
+    end
+
+    function ModelInputNode(cpd::CPD, map2uqinputs::Dict{Symbol,UQInput})
+        ```Function for Root Node only```
+        parents = Vector{AbstractNode}()
+        if isa(cpd, RootCPD)
+            type = isa(cpd.distributions, NamedCategorical) ? "discrete" : "continuous"
+        elseif isa(cpd, CategoricalCPD)
+            type = isa(cpd.distributions[1], NamedCategorical) ? "discrete" : "continuous"
+        end
+        ModelInputNode(cpd, parents, type, map2uqinputs)
+    end
+    function ModelInputNode(cpd::CPD, parents::Vector{T}, map2uqinputs::Dict{Symbol,UQInput}) where {T<:AbstractNode}
+        if isa(cpd, RootCPD)
+            type = isa(cpd.distributions, NamedCategorical) ? "discrete" : "continuous"
+        elseif isa(cpd, CategoricalCPD)
+            type = isa(cpd.distributions[1], NamedCategorical) ? "discrete" : "continuous"
+        end
+        ##TODO add to log
+        f = x -> findmax(collect(values(x)), dims=1)[1][1]
+        discrete_parents = filter(x -> x.type == "discrete", parents)
+        continuous_parents = filter(x -> x.type == "continuous", parents)
+        discrete_parental_ncategories = f.(discrete_states.(discrete_parents))
+        isempty(discrete_parental_ncategories) ? discrete_parental_ncategories = [1] : discrete_parental_ncategories = discrete_parental_ncategories
+        continuous_parental_ncategories = length.(continuous_distributions.(continuous_parents))
+        isempty(continuous_parental_ncategories) ? continuous_parental_ncategories = [1] : continuous_parental_ncategories = continuous_parental_ncategories
+        theorical_parents_categories = prod(discrete_parental_ncategories) * prod(continuous_parental_ncategories)
+        if theorical_parents_categories != prod(cpd.parental_ncategories)
+            node_name = cpd.target
+            throw(DomainError(node_name, "number of assigned cpds id not equal to parental categories $theorical_parents_categories"))
+        else
+            ModelInputNode(cpd, parents, type, map2uqinputs)
+        end
+    end
+end
+
+
+
 
 function name(node::T) where {T<:AbstractNode}
     return node.cpd.target
 end
 
-# ``` 1) discrete_states returns:
-#             states for discrete nodes 
-#                 Vector{Dict{state_description_Symbol, state_number}}
-#     2) continuous_distributions returns:
-#             possible distributions for continuous nodes 
-#                 Vector{Dict{distribution_description, distribution_number}}
-# ```
+""" 
+    1) discrete_states returns:
+            states for discrete nodes 
+                Vector{Dict{state_description_Symbol, state_number}}
+    2) continuous_distributions returns:
+            possible distributions for continuous nodes 
+                Vector{Dict{distribution_description, distribution_number}}
+"""
 
 function discrete_states(node::AbstractNode)
     if node.type == "continuous"
@@ -118,15 +171,6 @@ function continuous_distributions(node::AbstractNode)
     end
 end
 
-
-# function states(node::AbstractNode)
-#     if node.cpd isa RootCPD
-#         return node.type == "discrete" ? node.cpd.distributions.map.n2d : "this is a continuous node"
-#     else
-#         return node.type == "discrete" ? node.cpd.distributions[1].map.n2d : "this is a continuous node"
-#     end
-# end
-
 function get_discrete_parents(node::T) where {T<:AbstractNode}
     discrete_parents = copy(node.parents)
     return filter(x -> x.type == "discrete", discrete_parents)
@@ -145,37 +189,54 @@ function nodes_split(nodes::Vector{T}) where {T<:AbstractNode}
     return discrete_parents, continuous_nonroot_parents, continuous_root_parents
 end
 
-
-function get_states_combination(nodes::Vector{T}) where {T<:AbstractNode}
-    continuous = name.(filter(x -> x.type == "continuous", nodes))
-    if ~isempty(continuous)
-        filter(x -> x.type == "continuous", nodes)
-        println("$continuous are continuous nodes => Not taken into account")
+function get_statesordistributions(node::T) where {T<:AbstractNode}
+    if node.type == "continuous"
+        return Dict(name(node) => [node.cpd.distributions])
+    elseif node.type == "discrete"
+        ~isa(node.cpd, RootCPD) ? result = Dict(name(node) => collect(values(node.cpd.distributions[1].map.d2n))) : result = Dict(name(node) => collect(values(node.cpd.distributions.map.d2n)))
+        return result
     end
-    discrete_nodes = filter(x -> x.type == "discrete", nodes)
-    nodes_states = Vector{Dict{Symbol,Vector}}()
-    to_combine = []
-    nodes_combinations = Vector{Tuple{Symbol}}()
-    for node in discrete_nodes
-        if node.cpd isa RootCPD
-            push!(nodes_states, Dict(name(node) => collect(values(node.cpd.distributions.map.d2n))))
-            push!(to_combine, collect(values(node.cpd.distributions.map.d2n)))
-        else
-            push!(nodes_states, Dict(name(node) => collect(values(node.cpd.distributions[1].map.d2n))))
-            push!(to_combine, collect(values(node.cpd.distributions[1].map.d2n)))
-        end
-    end
-    nodes_combinations = collect(Iterators.product(to_combine...))
-    return nodes_states, nodes_combinations
 end
+
+function get_combinations(nodes::Vector{T}) where {T<:AbstractNode}
+    states_dictionary = get_statesordistributions.(nodes)
+    to_combine = []
+    for node in states_dictionary
+        push!(to_combine, collect(values(node))[1])
+    end
+    return vec(collect(Iterators.product(to_combine...)))
+end
+
+# function get_states_combination(nodes::Vector{T}) where {T<:AbstractNode}
+#     continuous = name.(filter(x -> x.type == "continuous", nodes))
+#     if ~isempty(continuous)
+#         filter(x -> x.type == "continuous", nodes)
+#         println("$continuous are continuous nodes => Not taken into account")
+#     end
+#     discrete_nodes = filter(x -> x.type == "discrete", nodes)
+#     nodes_states = Vector{Dict{Symbol,Vector}}()
+#     to_combine = []
+#     nodes_combinations = Vector{Tuple{Symbol}}()
+#     for node in discrete_nodes
+#         if node.cpd isa RootCPD
+#             push!(nodes_states, Dict(name(node) => collect(values(node.cpd.distributions.map.d2n))))
+#             push!(to_combine, collect(values(node.cpd.distributions.map.d2n)))
+#         else
+#             push!(nodes_states, Dict(name(node) => collect(values(node.cpd.distributions[1].map.d2n))))
+#             push!(to_combine, collect(values(node.cpd.distributions[1].map.d2n)))
+#         end
+#     end
+#     nodes_combinations = vec(collect(Iterators.product(to_combine...)))
+#     return nodes_combinations
+# end
 
 function get_states_mapping_dict(nodes::Vector{T}) where {T<:AbstractNode}
     mapping = Dict{AbstractNode,Dict{}}()
     for node in nodes
-        if node.cpd isa RootCPD
-            mapping[node] = node.cpd.distributions.map.n2d
-        else
-            mapping[node] = node.cpd.distributions[1].map.n2d
+        if node.type == "discrete"
+            isa(node.cpd, RootCPD) ? mapping[name(node)] = node.cpd.distributions.map.n2d : mapping[name(node)] = node.cpd.distributions[1].map.n2d
+        elseif node.type == "continuous"
+            mapping[name(node)] = Dict(node.cpd.distributions => node.cpd.distributions)
         end
     end
     return mapping
@@ -239,7 +300,6 @@ function map_integer_to_state(vector_to_be_mapped::Vector, nodes::Vector{T}) whe
     return new_dict
 end
 
-
 function get_discreteparents_states_combinations(node::T) where {T<:AbstractNode}
     discrete_parents = get_discrete_parents(node)
     return get_states_combination(discrete_parents)
@@ -249,43 +309,3 @@ function get_discreteparents_states_mapping_dict(node::T) where {T<:AbstractNode
     discrete_parents = get_discrete_parents(node)
     return get_states_mapping_dict(discrete_parents)
 end
-
-
-# function get_common_parents(nodes::Vector{T}) where {T<:AbstractNode}
-#     all_parents_dict = Dict{Any,Vector{Any}}()
-#     all_parents_vector = Vector{Any}()
-#     for node in nodes
-#         all_parents_dict[name(node)] = [name(i) for i in node.parents]
-#         for grandparent in node.parents
-#             push!(all_parents_vector, name(grandparent))
-#         end
-#     end
-#     unique!(all_parents_vector)
-#     final_dict = Dict{Any,Vector{Any}}()
-#     for el in all_parents_vector
-#         final_vect_i = Vector{Any}()
-#         for (k, v) in all_parents_dict
-#             if el in v
-#                 push!(final_vect_i, k)
-#             end
-#         end
-#         if length(final_vect_i) > 1
-#             final_dict[el] = final_vect_i
-#         end
-#     end
-#     return final_dict
-# end
-
-# function get_cpd_dict(node::Node)
-#     if length(node.parents) != 0
-#         cpds_dict = Dict{Tuple,Union{CPD,Distribution}}()
-#         combinations = vec(get_discreteparents_states_combinations(node)[2])
-#         ordered_dict = sort(map_state_to_integer(combinations, node))
-#         for i in range(1, length(node.cpd.distributions))
-#             cpds_dict[collect(keys(ordered_dict))[i]] = node.cpd.distributions[i]
-#         end
-#         return sort(cpds_dict)
-#     else
-#         return Dict(tuple(undef) => node.cpd.distributions)
-#     end
-# end
