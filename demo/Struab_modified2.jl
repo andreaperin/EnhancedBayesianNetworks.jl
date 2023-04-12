92157fb503419c4e7b9beb2b6e139819968a34c4
andrea perin <andrea.perin@irz.uni-hannover.de>
2023-04-12 11:36:55 +0200

    Modify  demo Struab

--- demo/Struab_modified2.jl
+++ demo/Struab_modified2.jl
@@ -48,13 +48,27 @@ emission2 = [
 parameters_vector = [emission1, emission2]
 node_emission = RootNode(CPD_emission, parameters_vector)
 
-function cpd_r_given_o(o)
-    return r -> cdf(Normal(), (ln(r) - o * sqrt(ρᵣ) - λᵣ) / sqrt(ζᵣ^2 - ρᵣ))
-end
 
-model_o = ModelWithName(:model_o, [Model(cpd_r_given_o, :o)])
-o_CPD = FunctionalCPD(:O, name.([Uᵣ_node]), [1], [model_o])
-o_node = FunctionalNode(o_CPD, [Uᵣ_node], "continuous")
+function cpd_f_given_ur(ur)
+    return r -> cdf(Normal(), (ln(r) - ur * sqrt(ρᵣ) - λᵣ) / sqrt(ζᵣ^2 - ρᵣ))
+end
+model_f = ModelWithName(:model_f, [Model(cpd_f_given_ur, :f)])
+f_CPD = FunctionalCPD(:f, name.([Uᵣ_node]), [1], [model_f])
+f_node = FunctionalNode(f_CPD, [Uᵣ_node], "continuous")
+
+function cpd_o_given_f(f)
+    return r -> cdf(Normal(), (ln(r) - f * sqrt(ρᵣ) - λᵣ) / sqrt(ζᵣ^2 - ρᵣ))
+end
+model_o1 = ModelWithName(:model_o1, [Model(cpd_o_given_f, :o1)])
+o1_CPD = FunctionalCPD(:o1, name.([f_node]), [1], [model_o1])
+o1_node = FunctionalNode(o1_CPD, [f_node], "continuous")
+
+function cpd_o_given_ur(ur)
+    return r -> cdf(Normal(), (ln(r) - ur * sqrt(ρᵣ) - λᵣ) / sqrt(ζᵣ^2 - ρᵣ))
+end
+model_o2 = ModelWithName(:model_o2, [Model(cpd_o_given_ur, :o2)])
+o2_CPD = FunctionalCPD(:o2, name.([Uᵣ_node]), [1], [model_o2])
+o2_node = FunctionalNode(o2_CPD, [Uᵣ_node], "continuous")
 
 ## DiscreteNode 2
 # emission_ts1 = NamedCategorical([:nothappen, :happen], [0.0, 1.0])
@@ -80,18 +94,27 @@ parental_ncategoriesᵣ = Vector{Int}()
 
 
 ## Function for returning cpd of node Rᵢ ∀ i in [1;5]
-function cpd_r_given_u(uᵣ)
-    return r -> cdf(Normal(), (ln(r) - uᵣ * sqrt(ρᵣ) - λᵣ) / sqrt(ζᵣ^2 - ρᵣ))
+function cpd_r_given_o1(o1)
+    return r -> cdf(Normal(), (ln(r) - o1 * sqrt(ρᵣ) - λᵣ) / sqrt(ζᵣ^2 - ρᵣ))
 end
 
 ## node R₁
-model₁ = ModelWithName(:model1, [Model(cpd_r_given_o, :R₁)])
-R₁_CPD = FunctionalCPD(:R₁, name.([o_node]), parental_ncategoriesᵣ, [model₁])
-R₁_node = FunctionalNode(R₁_CPD, [o_node], "continuous")
+model₁ = ModelWithName(:model1, [Model(cpd_r_given_o1, :R₁)])
+R₁_CPD = FunctionalCPD(:R₁, name.([o1_node]), parental_ncategoriesᵣ, [model₁])
+R₁_node = FunctionalNode(R₁_CPD, [o1_node], "continuous")
+
+function cpd_r_given_o2(o2)
+    return r -> cdf(Normal(), (ln(r) - o2 * sqrt(ρᵣ) - λᵣ) / sqrt(ζᵣ^2 - ρᵣ))
+end
+
+model₂ = ModelWithName(:model2, [Model(cpd_r_given_o2, :R₂)])
+R₂_CPD = FunctionalCPD(:R₂, name.([o2_node]), parental_ncategoriesᵣ, [model₂])
+R₂_node = FunctionalNode(R₂_CPD, [o2_node], "continuous")
+
 ## node R₂
-model₂ = ModelWithName(:model2, [Model(cpd_r_given_u, :R₂)])
-R₂_CPD = FunctionalCPD(:R₂, name.(parentsᵣ), parental_ncategoriesᵣ, [model₂])
-R₂_node = FunctionalNode(R₂_CPD, parentsᵣ, "continuous")
+function cpd_r_given_u(ur)
+    return r -> cdf(Normal(), (ln(r) - ur * sqrt(ρᵣ) - λᵣ) / sqrt(ζᵣ^2 - ρᵣ))
+end
 ## node R₃
 model₃ = ModelWithName(:model3, [Model(cpd_r_given_u, :R₃)])
 R₃_CPD = FunctionalCPD(:R₃, name.(parentsᵣ), parental_ncategoriesᵣ, [model₃])
@@ -145,8 +168,8 @@ E_CPD = FunctionalCPD(:E, name.(parentsₑ), parental_ncategoriesₑ, [modelₑ1
 E_node = FunctionalNode(E_CPD, parentsₑ, "discrete")
 
 # ebn = EnhancedBayesNet([Uᵣ_node, V_node, H_node, R₁_node, R₂_node, R₃_node, R₄_node, R₅_node, E_node, node_emission, node_timescenario])
-ebn = EnhancedBayesNet([Uᵣ_node, o_node, V_node, H_node, R₁_node, R₂_node, R₃_node, R₄_node, R₅_node, E_node, node_emission, node_timescenario])
-
+ebn = EnhancedBayesNet([Uᵣ_node, f_node, o1_node, o2_node, V_node, H_node, R₁_node, R₂_node, R₃_node, R₄_node, R₅_node, E_node, node_emission, node_timescenario])
+show(ebn)
 groups = markov_envelopes(ebn)
 rdag, dag_names = _reduce_ebn_to_rbn(ebn)
 graphplot(
@@ -165,14 +188,14 @@ r_nodes = _get_node_in_rbn(ebn)
 # nodes_to_be_evaluated = filter(x -> isa(x, FunctionalNode), r_nodes)
 # a, b = _get_ancestors_distribution_4sampling(nodes_to_be_evaluated[1])
 # # node = nodes_to_be_evaluated[1]
-empty_srp = empty_srp_table_with_evidence[1]
-# parent_node = V_node
-# a = _build_srp_single_node_single_evidence(ebn, empty_srp.evidence, parent_node)
-single_struc_table = empty_srp
-node = E_node
+# empty_srp = empty_srp_table_with_evidence[1]
+# # parent_node = V_node
+# # a = _build_srp_single_node_single_evidence(ebn, empty_srp.evidence, parent_node)
+# single_struc_table = empty_srp
+# node = E_node
 
 
-uqinputs = _build_uqinputs_vector_single_evidence(ebn, empty_srp, E_node)
+# uqinputs = _build_uqinputs_vector_single_evidence(ebn, empty_srp, E_node)
 
 a = _functional_node_after_reduction(ebn, empty_srp_table_with_evidence, E_node)
 # a.evidence_table[1].srp[2][2]
