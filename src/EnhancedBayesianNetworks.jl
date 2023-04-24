1d6e8c9203514df0bfbb400cccf220f960f1413a
Andrea Perin <andrea.perin@irz.uni-hannover.de>
2023-04-24 17:28:42 +0200

    Add enhanced bayesian network structure and its initial function and test

--- src/EnhancedBayesianNetworks.jl
+++ src/EnhancedBayesianNetworks.jl
@@ -1,7 +1,9 @@
 module EnhancedBayesianNetworks
 
+using GraphRecipes
 using Graphs
 using Reexport
+
 
 @reexport using UncertaintyQuantification
 @reexport using OrderedCollections
@@ -17,14 +19,16 @@ export ContinuousStandardNode
 export DiscreteFunctionalNode
 export DiscreteRootNode
 export DiscreteStandardNode
+export EnhancedBayesianNetwork
 export RootNode
 export StandardNode
 
 
 # Methods
-# export _get_states
-
+export show
 
 include("nodes/nodes.jl")
 include("util/probabilities.jl")
+include("ebn.jl")
+
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,9 @@
 using Test
 
 using EnhancedBayesianNetworks
+using Graphs
 
+include("ebn.jl")
 include("nodes/root.jl")
 include("nodes/standard.jl")
 include("nodes/functional.jl")
