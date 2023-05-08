2a50f7a479ab146aaa854f69749b94fffb9b3f14
Andrea Perin <andrea.perin1992@gmail.com>
2023-05-08 19:12:14 +0200

    Correction for Straub Example

--- demo/Straub1_Copula.jl
+++ demo/Straub1_Copula.jl
@@ -1,33 +1,24 @@
-include("../src/bn.jl")
+using EnhancedBayesianNetworks
+using Plots
+
+
+## Fake node
+time = DiscreteRootNode(:t, Dict(:first => 0.5, :second => 0.5), Dict(:first => [Parameter(1, :useless)], :second => [Parameter(1, :useless)]))
 
 ## R Nodes Definition as RootNodes
 λᵣ = 150
-σᵣ = 0.2
+σᵣ = 0.2 * 150
 ρᵣ = 0.3
-log_λᵣ = log(λᵣ^2 / (sqrt(λᵣ^2 + σᵣ^2)))
+log_λᵣ = log((λᵣ^2) / (sqrt(λᵣ^2 + σᵣ^2)))
 log_σᵣ = sqrt(log(1 + σᵣ^2 / λᵣ^2))
 
 R_dist = LogNormal(log_λᵣ, log_σᵣ)
 
-R1 = RandomVariable(R_dist, :R1)
-R1_cpd = RootCPD(:R1, [R_dist])
-R1_node = RootNode(R1_cpd)
-
-R2 = RandomVariable(R_dist, :R2)
-R2_cpd = RootCPD(:R2, [R_dist])
-R2_node = RootNode(R2_cpd)
-
-R3 = RandomVariable(R_dist, :R3)
-R3_cpd = RootCPD(:R3, [R_dist])
-R3_node = RootNode(R3_cpd)
-
-R4 = RandomVariable(R_dist, :R4)
-R4_cpd = RootCPD(:R4, [R_dist])
-R4_node = RootNode(R4_cpd)
-
-R5 = RandomVariable(R_dist, :R5)
-R5_cpd = RootCPD(:R5, [R_dist])
-R5_node = RootNode(R5_cpd)
+R1 = ContinuousRootNode(:R1, R_dist)
+R2 = ContinuousRootNode(:R2, R_dist)
+R3 = ContinuousRootNode(:R3, R_dist)
+R4 = ContinuousRootNode(:R4, R_dist)
+R5 = ContinuousRootNode(:R5, R_dist)
 
 
 ## Correlation Node
@@ -38,26 +29,98 @@ correlationvector4 = [ρᵣ ρᵣ ρᵣ 1 ρᵣ]
 correlationvector5 = [ρᵣ ρᵣ ρᵣ ρᵣ 1]
 correlationMatrix = vcat(correlationvector1, correlationvector2, correlationvector3, correlationvector4, correlationvector5)
 c = GaussianCopula(correlationMatrix)
-jd_dist = JointDistribution([R1, R2, R3, R4, R5], c)
-jd_parents = [R1_node, R2_node, R3_node, R4_node, R5_node]
-jd_CDP = StdCPD(:jd, name.(jd_parents), jd_dist)
-jd_node = StdNode(jd_CDP, jd_parents)
+e = Vector{Tuple{Symbol,AbstractNode}}()
+dist = JointDistribution(get_randomvariable.([R1, R2, R3, R4, R5], [e, e, e, e, e]), c)
+jd_dist = OrderedDict(
+    [:first] => dist,
+    [:second] => dist
+)
+jd_parents = [time, R1, R2, R3, R4, R5]
+jd_node = ContinuousStandardNode(:jd, jd_parents, jd_dist)
+
+
+## External Forces
+
+θ = 0.4 * 50 * √(6) / π
+μ = 50 - θ * Base.MathConstants.eulergamma
+H = ContinuousRootNode(:H, Gumbel(μ, θ))
+
+θ = (0.2)^2 * 60
+α = 60 / θ
+V = ContinuousRootNode(:V, Gamma(α, θ))
 
 ## Output Node
 # Failure Functions
 failure_1 = df -> df.R1 .+ df.R2 .+ df.R4 .+ df.R5 - 5 .* df.H
 failure_2 = df -> df.R2 .+ 2 .* df.R3 .+ df.R4 - 5 .* df.V
 failure_3 = df -> df.R1 .+ 2 .* df.R3 .+ 2 .* df.R4 .+ df.R5 - 5 .* df.H - 5 .* df.V
-performance = df -> minimum([df.f1, df.f2, df.f3])
-
 model1 = Model(failure_1, :f1)
 model2 = Model(failure_2, :f2)
 model3 = Model(failure_3, :f3)
-perf = Model(performance, :output)
 
-models = [model1, model2, model3, perf]
+models = OrderedDict(
+    [:first] => [model1, model2, model3],
+    [:second] => [model1, model2, model3]
+)
+performances = OrderedDict(
+    [:first] => df -> minimum(hcat(df.f1, df.f2, df.f3), dims=2),
+    [:second] => df -> minimum(hcat(df.f1, df.f2, df.f3), dims=2)
+)
+simulations = OrderedDict(
+    [:first] => MonteCarlo(10^5),
+    [:second] => MonteCarlo(10^6)
+)
 
-model = ModelWithName(:failuremodes, models)
-output_parents = [jd_node]
-output_CPD = FunctionalCPD(:out, name.(output_parents), model)
 
+E = DiscreteFunctionalNode(:E, [H, V, time, jd_node], models, performances, simulations)
+
+ebn = EnhancedBayesianNetwork([H, V, R1, R2, R3, R4, R5, jd_node, time, E])
+EnhancedBayesianNetworks.plot(ebn)
+
+rbns = reduce_ebn_markov_envelopes(ebn)
+
+evaluate_rbn(rbns)
+
+
+
+### Structural Reliability ProbabilisticGraphicalModel
+λᵣ = 150
+σᵣ = 0.2 * 150
+ρᵣ = 0.3
+log_λᵣ = log((λᵣ^2) / (sqrt(λᵣ^2 + σᵣ^2)))
+log_σᵣ = sqrt(log(1 + σᵣ^2 / λᵣ^2))
+R1 = RandomVariable(LogNormal(log_λᵣ, log_σᵣ), :R1)
+R2 = RandomVariable(LogNormal(log_λᵣ, log_σᵣ), :R2)
+R3 = RandomVariable(LogNormal(log_λᵣ, log_σᵣ), :R3)
+R4 = RandomVariable(LogNormal(log_λᵣ, log_σᵣ), :R4)
+R5 = RandomVariable(LogNormal(log_λᵣ, log_σᵣ), :R5)
+
+θ = 0.4 * 50 * √(6) / π
+μ = 50 - θ * Base.MathConstants.eulergamma
+H = RandomVariable(Gumbel(μ, θ), :H)
+
+θ = (0.2)^2 * 60
+α = 60 / θ
+V = RandomVariable(Gamma(α, θ), :V)
+
+correlationvector1 = [1 ρᵣ ρᵣ ρᵣ ρᵣ]
+correlationvector2 = [ρᵣ 1 ρᵣ ρᵣ ρᵣ]
+correlationvector3 = [ρᵣ ρᵣ 1 ρᵣ ρᵣ]
+correlationvector4 = [ρᵣ ρᵣ ρᵣ 1 ρᵣ]
+correlationvector5 = [ρᵣ ρᵣ ρᵣ ρᵣ 1]
+correlationMatrix = vcat(correlationvector1, correlationvector2, correlationvector3, correlationvector4, correlationvector5)
+c = GaussianCopula(correlationMatrix)
+jd = JointDistribution([R1, R2, R3, R4, R5], c)
+
+failure_1 = df -> df.R1 .+ df.R2 .+ df.R4 .+ df.R5 - 5 .* df.H
+failure_2 = df -> df.R2 .+ 2 .* df.R3 .+ df.R4 - 5 .* df.V
+failure_3 = df -> df.R1 .+ 2 .* df.R3 .+ 2 .* df.R4 .+ df.R5 - 5 .* df.H - 5 .* df.V
+model1 = Model(failure_1, :f1)
+model2 = Model(failure_2, :f2)
+model3 = Model(failure_3, :f3)
+perf = Model(df -> minimum([df.f1, df.f2, df.f3]), :output)
+
+model = [model1, model2, model3, perf]
+inputs = [H, V, jd]
+
+res = probability_of_failure(model, df -> minimum(hcat(df.f1, df.f2, df.f3), dims=2), inputs, MonteCarlo(10^6))[1]
