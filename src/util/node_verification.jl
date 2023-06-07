245e6fea63912a65124b029f3a154f0f94e92e2d
Andrea Perin <andrea.perin1992@gmail.com>
2023-06-07 18:10:16 +0200

    Correction for probability verification caused by numerical issue.

--- src/util/node_verification.jl
+++ src/util/node_verification.jl
@@ -1,7 +1,7 @@
 function verify_probabilities(states::Dict{Symbol,<:Real})
     any(values(states) .< 0.0) && error("Probabilites must be nonnegative")
     any(values(states) .> 1.0) && error("Probabilites must be less or equal to 1.0")
-    sum(values(states)) != 1 && error("Probabilites must sum up to 1.0")
+    !isapprox(sum(values(states)), 1) && error("Probabilites must sum up to 1.0")
 end
 
 function verify_parameters(states::Dict{Symbol,<:Real}, parameters::Dict{Symbol,Vector{Parameter}})
