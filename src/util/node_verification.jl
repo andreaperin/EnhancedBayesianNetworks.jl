70cc25b03d98fabc9fe63230a06823a26df9cff2
Andrea Perin <andrea.perin1992@gmail.com>
2023-08-22 16:45:32 +0200

    Correction for parents state check of FunctionalNodes

--- src/util/node_verification.jl
+++ src/util/node_verification.jl
@@ -13,6 +13,7 @@ end
 function verify_functionalnode_parents(parents::Vector{<:AbstractNode})
     discrete_parents = filter(x -> isa(x, DiscreteNode), parents)
     discrete_parents_states = vcat([collect(keys(x.states)) for x in discrete_parents]...)
+    discrete_parents_states = mapreduce(x -> EnhancedBayesianNetworks._get_states(x), vcat, discrete_parents)
 
     any(isempty.([x.parameters for x in discrete_parents])) && error("all discrete parents of a functional node must have a non-empty parameters dictionary")
 
