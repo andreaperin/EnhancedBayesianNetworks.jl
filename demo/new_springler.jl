6d25b3b090682d8e445d774b754863c54b9b7996
Andrea Perin <andrea.perin1992@gmail.com>
2023-11-09 14:46:12 +0100

    new_spingler demo update for semplification branch.

--- demo/new_springler.jl
+++ demo/new_springler.jl
@@ -3,7 +3,7 @@ using EnhancedBayesianNetworks
 
 cloudy = DiscreteRootNode(:cloudy, Dict(:cloudy => 0.5, :sunny => 0.5))
 
-random = ContinuousRootNode(:random, Normal(), [[-0.5, 0], [0, 0.5]])
+random = ContinuousRootNode(:random, Normal(), ExactDiscretization([-0.5, 0, 0.5]))
 
 sprinkler_name = :sprinkler
 sprinkler_parents = [cloudy]
@@ -29,37 +29,16 @@ wetgrass_name = :wetgrass
 
 wetgrass_parents = [random, rain_node, sprinkler_node]
 wetgrass_model1 = Model(df -> df.random .^ 2 .* df.rn .+ df.sp, :final_state)
-wetgrass_models = Dict(
-    [:rain, :on] => [wetgrass_model1],
-    [:rain, :off] => [wetgrass_model1],
-    [:not_rain, :on] => [wetgrass_model1],
-    [:not_rain, :off] => [wetgrass_model1]
-)
-wetgrass_simulations = Dict(
-    [:rain, :on] => MonteCarlo(200),
-    [:rain, :off] => MonteCarlo(200),
-    [:not_rain, :on] => MonteCarlo(200),
-    [:not_rain, :off] => MonteCarlo(200)
-)
-wetgrass_performances = Dict(
-    [:rain, :on] => df -> df.final_state,
-    [:rain, :off] => df -> df.final_state,
-    [:not_rain, :on] => df -> df.final_state,
-    [:not_rain, :off] => df -> df.final_state
-)
+wetgrass_models = [wetgrass_model1]
+wetgrass_simulations = MonteCarlo(200)
+wetgrass_performances = df -> df.final_state
 wetgrass_node = DiscreteFunctionalNode(wetgrass_name, wetgrass_parents, wetgrass_models, wetgrass_performances, wetgrass_simulations)
 
-# wetfloor_name = :wetfloor
-# wetfloor_parents = [random, rain_node]
-# wetfloor_model1 = Model(df -> df.random .^ 2 .* df.rn, :final_floor)
-# wetfloor_models = Dict(
-#     [:rain] => [wetfloor_model1],
-#     [:not_rain] => [wetfloor_model1]
-# )
-# wetfloor_simulations = Dict(
-#     [:rain] => MonteCarlo(200),
-#     [:not_rain] => MonteCarlo(200)
-# )
+wetfloor_name = :wetfloor
+wetfloor_parents = [random, rain_node]
+wetfloor_model1 = Model(df -> df.random .^ 2 .* df.rn, :final_floor)
+wetfloor_models = [wetfloor_model1]
+wetfloor_simulations = MonteCarlo(200)
 
 wetfloor_node = ContinuousFunctionalNode(wetfloor_name, wetfloor_parents, wetfloor_models, wetfloor_simulations)
 
@@ -67,15 +46,4 @@ nodes = [random, cloudy, rain_node, sprinkler_node, wetgrass_node, wetfloor_node
 
 ebn = EnhancedBayesianNetwork(nodes)
 
-# p = EnhancedBayesianNetworks.plot(ebn)
-# savefig("/Users/andreaperin_macos/Documents/PhD/3_Academic/Papers_Presentations/Conferences/2023_ESREL/ExtendedAbstract-Template/imgs/ebn2_spinkler.png")
-
-
-rbn1 = reduce_ebn_standard(ebn)
-# EnhancedBayesianNetworks.plot(rbn1)
-# savefig("/Users/andreaperin_macos/Documents/PhD/3_Academic/Papers_Presentations/Conferences/2023_ESREL/ExtendedAbstract-Template/imgs/rbn2_sprinkler.png")
-
-
-a = evaluate_ebn(ebn)
-
-# bn = BayesianNetwork(rbn1)
+a = evaluate!(ebn)
