31f000f53be08a74fe93dd2868905cd8a10d4a7d
Andrea Perin <andrea.perin1992@gmail.com>
2023-12-05 17:08:08 +0100

    Add new evaluate with new_routine

--- src/ebn/ebn.jl
+++ src/ebn/ebn.jl
@@ -8,3 +8,19 @@ include("reduction/new_routine.jl")
 include("srp/srp.jl")
 include("cpd/conditionalprobabilitydistribution.jl")
 include("bayesnet.jl")
+
+function evaluate!(ebn::EnhancedBayesianNetwork)
+    ## transfer all possible continuous functional node's model to their discrete functional children
+    new_ebn = deepcopy(ebn)
+    new_ebn = transfer_continuous(new_ebn)
+    ## determine functional nodes to evaluate
+    functional_nodes = [i.name for i in filter(x -> isa(x, FunctionalNode), new_ebn.nodes)]
+    e_ebn = deepcopy(new_ebn)
+    ## evluate all functional nodes
+    while !isempty(functional_nodes)
+        e_ebn, evaluated_nodes = _evaluate_single_layer(e_ebn)
+        functional_nodes = setdiff(functional_nodes, evaluated_nodes)
+    end
+    ## reduce obtained network 
+    return reduce!(e_ebn)
+end
