0e38e6bdaaf9d2fdefeee77d123e6a8f0ea5fbc2
Andrea Perin <andrea.perin1992@gmail.com>
2023-12-05 11:10:53 +0100

    Add correction for checking parents of a ContFunNode with DiscFunNode (no states) as parent!

--- src/util/node_verification.jl
+++ src/util/node_verification.jl
@@ -19,12 +19,15 @@ end
 
 function verify_functionalnode_parents(parents::Vector{<:AbstractNode})
     discrete_parents = filter(x -> isa(x, DiscreteNode), parents)
-    discrete_parents_states = vcat([collect(keys(x.states)) for x in discrete_parents]...)
-    discrete_parents_states = mapreduce(x -> EnhancedBayesianNetworks._get_states(x), vcat, discrete_parents)
+    discrete_parents = filter(x -> !isa(x, FunctionalNode), discrete_parents)
+    if !isempty(discrete_parents)
+        discrete_parents_states = vcat([collect(keys(x.states)) for x in discrete_parents]...)
+        discrete_parents_states = mapreduce(x -> _get_states(x), vcat, discrete_parents)
 
-    any(isempty.([x.parameters for x in discrete_parents])) && error("all discrete parents of a functional node must have a non-empty parameters dictionary")
+        any(isempty.([x.parameters for x in discrete_parents])) && error("all discrete parents of a functional node must have a non-empty parameters dictionary")
 
-    unique(discrete_parents_states) != discrete_parents_states && error("all discrete parents of a functional node must have different named states")
+        unique(discrete_parents_states) != discrete_parents_states && error("all discrete parents of a functional node must have different named states")
+    end
 end
 
 function Base.isequal(node1::AbstractNode, node2::AbstractNode)
