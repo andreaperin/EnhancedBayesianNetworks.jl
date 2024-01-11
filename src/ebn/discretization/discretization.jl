ce13bd83e961e8874c81ccebbe4f8a63ef974c39
Andrea Perin <andrea.perin1992@gmail.com>
2024-01-11 17:34:15 +0100

    Add Jasper's suggestion for ```discretization.jl```

--- src/ebn/discretization/discretization.jl
+++ src/ebn/discretization/discretization.jl
@@ -1,12 +1,12 @@
 abstract type AbstractDiscretization end
 
 
-``` ExactDiscretization
+""" ExactDiscretization
 
     Used for ContinuousRootNode whenever evidence can be available on them.
         intervals: vector of Float64 that discretize initial distribution support
 
-```
+"""
 struct ExactDiscretization <: AbstractDiscretization
     intervals::Vector{<:Real}
 
@@ -18,6 +18,8 @@ struct ExactDiscretization <: AbstractDiscretization
     end
 end
 
+ExactDiscretization() = ExactDiscretization(Vector{Real}())
+
 function Base.isequal(discretization1::ExactDiscretization, discretization2::ExactDiscretization)
     discretization1.intervals == discretization2.intervals
 end
@@ -27,30 +29,31 @@ function Base.hash(discretization::ExactDiscretization, h::UInt)
     return h
 end
 
-ExactDiscretization() = ExactDiscretization(Vector{Real}())
 
-``` ApproximatedDiscretization
+""" ApproximatedDiscretization
 
     Used for continuous Non-Root nodes whenever evidence can be available on them.
         intervals: vector of Float64 that discretize initial distribution support
         sigma: variance of the normal distribution used for appriximate initial continuous distribution
 
-```
+"""
 struct ApproximatedDiscretization <: AbstractDiscretization
     intervals::Vector{<:Real}
-    sigma::Union{Real,Nothing}
+    sigma::Real
 
-    function ApproximatedDiscretization(intervals::Vector{<:Real}, sigma::Union{Real,Nothing})
-        if sort(intervals) != intervals
+    function ApproximatedDiscretization(intervals::Vector{<:Real}, sigma::Real)
+        if !issorted(intervals)
             error("interval values $intervals are not sorted")
-        elseif !isa(sigma, Nothing) && sigma ≤ 0
+        elseif sigma < 0
             error("variance must be positive")
-        elseif !isa(sigma, Nothing) && sigma > 2
+        elseif sigma > 2
             @warn "Selected variance values $sigma can be too big, and the approximation not realistic"
         end
         new(intervals, sigma)
     end
 end
+
+ApproximatedDiscretization() = ApproximatedDiscretization(Vector{Real}(), 0)
 
 function Base.isequal(discretization1::ApproximatedDiscretization, discretization2::ApproximatedDiscretization)
     discretization1.intervals == discretization2.intervals && discretization1.sigma == discretization2.sigma
@@ -61,5 +64,3 @@ function Base.hash(discretization::ApproximatedDiscretization, h::UInt)
     h = hash(discretization.sigma, h)
     return h
 end
-
-ApproximatedDiscretization() = ApproximatedDiscretization(Vector{Real}(), nothing)
