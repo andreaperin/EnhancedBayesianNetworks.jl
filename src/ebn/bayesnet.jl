da21bf2f8cf92dda0ab75ab3ced4a9bfdbc7e5ba
Andrea Perin <andrea.perin1992@gmail.com>
2024-10-11 12:02:46 +0200

    Add the subtype <:AbstractNode when called into an AbstractVector

--- src/ebn/bayesnet.jl
+++ src/ebn/bayesnet.jl
@@ -3,7 +3,7 @@ struct BayesianNetwork <: ProbabilisticGraphicalModel
     nodes::Vector{<:DiscreteNode}
     name_to_index::Dict{Symbol,Int}
 
-    function BayesianNetwork(dag::DiGraph, nodes::Vector{AbstractNode}, name_to_index::Dict{Symbol,Int})
+    function BayesianNetwork(dag::DiGraph, nodes::Vector{<:AbstractNode}, name_to_index::Dict{Symbol,Int})
         if any([isa(x, FunctionalNode) for x in nodes])
             error("Network needs to be evaluated first")
         else
