e269c40995d36cfb52121099ea7faf1979fe504d
Andrea Perin <andrea.perin1992@gmail.com>
2024-11-01 11:29:28 +0100

    Add evaluate with envelopes. Still missing dispatch to correct network after reduce!

--- test/ebn/evaluate/evaluate_net.jl
+++ test/ebn/evaluate/evaluate_net.jl
@@ -74,68 +74,113 @@
         @test all(isa.(net1.nodes, DiscreteNode))
     end
 
-    # @testset "Add missing nodes to envelopes" begin
-    #     using .MathConstants: γ
-    #     Uᵣ = ContinuousRootNode(:Uᵣ, Normal())
-    #     μ_gamma = 60
-    #     cov_gamma = 0.2
-    #     M = DiscreteRootNode(:M, Dict(:new => 0.5, :old => 0.5))
-    #     α, θ = distribution_parameters(μ_gamma, μ_gamma * cov_gamma, Gamma)
-    #     V = ContinuousChildNode(:V, [M], Dict(
-    #         [:new] => Gamma(α, θ),
-    #         [:old] => Gamma(α - 1, 2.4)
-    #     ))
-    #     μ_gumbel = 50
-    #     cov_gumbel = 0.4
-    #     μ_loc, β = distribution_parameters(μ_gumbel, cov_gumbel * μ_gumbel, Gumbel)
-    #     H = ContinuousRootNode(:H, Gumbel(μ_loc, β))
-    #     function plastic_moment_capacities(uᵣ)
-    #         ρ = 0.5477
-    #         μ = 150
-    #         cov = 0.2
-    #         λ, ζ = distribution_parameters(μ, μ * cov, LogNormal)
-    #         normal_μ = λ + ρ * ζ * uᵣ
-    #         normal_std = sqrt((1 - ρ^2) * ζ^2)
-    #         exp(rand(Normal(normal_μ, normal_std)))
-    #     end
-    #     parents = [Uᵣ]
-    #     model1 = Model(df -> plastic_moment_capacities.(df.Uᵣ), :r1)
-    #     model2 = Model(df -> plastic_moment_capacities.(df.Uᵣ), :r2)
-    #     model3 = Model(df -> plastic_moment_capacities.(df.Uᵣ), :r3)
-    #     model4 = Model(df -> plastic_moment_capacities.(df.Uᵣ), :r4)
-    #     model5 = Model(df -> plastic_moment_capacities.(df.Uᵣ), :r5)
-    #     R1 = ContinuousFunctionalNode(:R1, parents, [model1], MonteCarlo(10^6))
-    #     R2 = ContinuousFunctionalNode(:R2, parents, [model2], MonteCarlo(10^6))
-    #     R3 = ContinuousFunctionalNode(:R3, parents, [model3], MonteCarlo(10^6))
-    #     R4 = ContinuousFunctionalNode(:R4, parents, [model4], MonteCarlo(10^6))
-    #     R5 = ContinuousFunctionalNode(:R5, parents, [model5], MonteCarlo(10^6))
-    #     function frame_model(r1, r2, r3, r4, r5, v, h)
-    #         g1 = r1 + r2 + r4 + r5 - 5 * h
-    #         g2 = r2 + 2 * r3 + r4 - 5 * v
-    #         g3 = r1 + 2 * r3 + 2 * r4 + r5 - 5 * h - 5 * v
-    #         return minimum([g1, g2, g3])
-    #     end
-    #     model = Model(df -> frame_model.(df.r1, df.r2, df.r3, df.r4, df.r5, df.V, df.H), :G)
-    #     performance = df -> df.G
-    #     simulation = MonteCarlo(10^6)
-    #     frame = DiscreteFunctionalNode(:E, [R1, R2, R3, R4, R5, V, H], [model], performance, simulation)
-    #     L = DiscreteChildNode(:L, [M], Dict(
-    #             [:old] => Dict(:yesL => 0.5, :noL => 0.5),
-    #             [:new] => Dict(:yesL => 0.2, :noL => 0.8),
-    #         ), Dict(:noL => [Parameter(1, :L)], :yesL => [Parameter(2, :L)]))
-    #     r9 = ContinuousRootNode(:R9, Normal())
-    #     model2 = Model(df -> df.L .^ 2 .* df.R9, :P)
-    #     frame2 = DiscreteFunctionalNode(:E2, [L, r9], [model2], df -> df.P, simulation)
-    #     nodes = [Uᵣ, M, V, H, R1, R2, R3, R4, R5, r9, frame, L, frame2]
-    #     ebn = EnhancedBayesianNetwork(nodes)
-    #     ebns = markov_envelope(ebn)
-    #     ebns = EnhancedBayesianNetworks._add_missing_nodes_to_envelope.(ebns)
-    #     @test issetequal([Uᵣ, M, V, H, R1, R2, R3, R4, R5, frame], ebns[1])
-    #     @test issetequal([r9, M, frame2, L], ebns[2])
+    @testset "Evaluate with envelopes" begin
+        using .MathConstants: γ
+        Uᵣ = ContinuousRootNode(:Uᵣ, Normal())
+        μ_gamma = 60
+        cov_gamma = 0.2
+        M = DiscreteRootNode(:M, Dict(:new => 0.5, :old => 0.5))
+        α, θ = distribution_parameters(μ_gamma, μ_gamma * cov_gamma, Gamma)
+        V = ContinuousChildNode(:V, Dict(
+            [:new] => Gamma(α, θ),
+            [:old] => Gamma(α - 1, 2.4)
+        ))
+        μ_gumbel = 50
+        cov_gumbel = 0.4
+        μ_loc, β = distribution_parameters(μ_gumbel, cov_gumbel * μ_gumbel, Gumbel)
+        H = ContinuousRootNode(:H, Gumbel(μ_loc, β))
+        function plastic_moment_capacities(uᵣ)
+            ρ = 0.5477
+            μ = 150
+            cov = 0.2
+            λ, ζ = distribution_parameters(μ, μ * cov, LogNormal)
+            normal_μ = λ + ρ * ζ * uᵣ
+            normal_std = sqrt((1 - ρ^2) * ζ^2)
+            exp(rand(Normal(normal_μ, normal_std)))
+        end
+        parents = [Uᵣ]
+        model1 = Model(df -> plastic_moment_capacities.(df.Uᵣ), :r1)
+        model2 = Model(df -> plastic_moment_capacities.(df.Uᵣ), :r2)
+        model3 = Model(df -> plastic_moment_capacities.(df.Uᵣ), :r3)
+        model4 = Model(df -> plastic_moment_capacities.(df.Uᵣ), :r4)
+        model5 = Model(df -> plastic_moment_capacities.(df.Uᵣ), :r5)
+        R1 = ContinuousFunctionalNode(:R1, [model1], MonteCarlo(10^6))
+        R2 = ContinuousFunctionalNode(:R2, [model2], MonteCarlo(10^6))
+        R3 = ContinuousFunctionalNode(:R3, [model3], MonteCarlo(10^6))
+        R4 = ContinuousFunctionalNode(:R4, [model4], MonteCarlo(10^6))
+        R5 = ContinuousFunctionalNode(:R5, [model5], MonteCarlo(10^6))
+        function frame_model(r1, r2, r3, r4, r5, v, h)
+            g1 = r1 + r2 + r4 + r5 - 5 * h
+            g2 = r2 + 2 * r3 + r4 - 5 * v
+            g3 = r1 + 2 * r3 + 2 * r4 + r5 - 5 * h - 5 * v
+            return minimum([g1, g2, g3])
+        end
+        model = Model(df -> frame_model.(df.r1, df.r2, df.r3, df.r4, df.r5, df.V, df.H), :G)
+        performance = df -> df.G
+        simulation = MonteCarlo(10^6)
 
-    #     res = evaluate_with_envelope(ebn)
-    #     @test isa(res, BayesianNetwork)
-    # end
+        frame = DiscreteFunctionalNode(:E, [model], performance, simulation)
+        L = DiscreteChildNode(:L, Dict(
+                [:old] => Dict(:yesL => 0.5, :noL => 0.5),
+                [:new] => Dict(:yesL => 0.2, :noL => 0.8),
+            ), Dict(:noL => [Parameter(1, :L)], :yesL => [Parameter(2, :L)]))
+        r9 = ContinuousRootNode(:R9, Normal())
+        model2 = Model(df -> df.L .^ 2 .* df.R9, :P)
+        frame2 = DiscreteFunctionalNode(:E2, [model2], df -> df.P, simulation)
+        nodes = [Uᵣ, M, V, H, R1, R2, R3, R4, R5, r9, frame, L, frame2]
+        ebn = EnhancedBayesianNetwork(nodes)
+        add_child!(ebn, M, V)
+        add_child!(ebn, Uᵣ, R1)
+        add_child!(ebn, Uᵣ, R2)
+        add_child!(ebn, Uᵣ, R3)
+        add_child!(ebn, Uᵣ, R4)
+        add_child!(ebn, Uᵣ, R5)
+        add_child!(ebn, R1, frame)
+        add_child!(ebn, R2, frame)
+        add_child!(ebn, R3, frame)
+        add_child!(ebn, R4, frame)
+        add_child!(ebn, R5, frame)
+        add_child!(ebn, V, frame)
+        add_child!(ebn, H, frame)
+        add_child!(ebn, M, L)
+        add_child!(ebn, L, frame2)
+        add_child!(ebn, r9, frame2)
+        order_net!(ebn)
+        envelopes = markov_envelope(ebn)
+
+        @test issetequal(EnhancedBayesianNetworks._add_root2envelope(ebn, envelopes[2]), [L, frame2, r9, M])
+        @test issetequal(EnhancedBayesianNetworks._add_root2envelope(ebn, envelopes[1]), envelopes[1])
+
+        ebn1 = EnhancedBayesianNetworks._build_envelope_edges(ebn, envelopes[1])
+        ebn2 = EnhancedBayesianNetworks._build_envelope_edges(ebn, envelopes[2])
+
+        @test ebn1.adj_matrix == sparse([
+            0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 1.0;
+            0.0 0.0 0.0 1.0 0.0 0.0 0.0 0.0 0.0 0.0;
+            0.0 0.0 0.0 0.0 1.0 1.0 1.0 1.0 1.0 0.0;
+            0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 1.0;
+            0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 1.0;
+            0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 1.0;
+            0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 1.0;
+            0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 1.0;
+            0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 1.0;
+            0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0])
+
+        @test ebn1.topology_dict == Dict(:M => 2, :H => 1, :R2 => 6, :R5 => 9, :V => 4, :R4 => 8, :R3 => 7, :R1 => 5, :E => 10, :Uᵣ => 3)
+        @test issetequal(ebn1.nodes, [Uᵣ, M, V, H, R1, R2, R3, R4, R5, frame])
+
+        @test ebn2.adj_matrix == sparse([
+            0.0 0.0 0.0 1.0;
+            0.0 0.0 1.0 0.0;
+            0.0 0.0 0.0 1.0;
+            0.0 0.0 0.0 0.0])
+        @test ebn2.topology_dict == Dict(:R9 => 1, :M => 2, :L => 3, :E2 => 4)
+        @test issetequal(ebn2.nodes, [r9, L, frame2, M])
+
+        eebns = evaluate_with_envelopes(ebn)
+        @test any(isa.(eebns[1].nodes, FunctionalNode)) == false
+        @test any(isa.(eebns[2].nodes, FunctionalNode)) == false
+    end
 
     @testset "No Ancestors case" begin
         root2 = ContinuousRootNode(:B, Normal())
