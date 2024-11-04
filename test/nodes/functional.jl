00e73084a3a51bde0e7b832aa9f7df8e3e95c244
Andrea Perin <andrea.perin1992@gmail.com>
2024-11-04 18:29:07 +0100

    Add wrap of models for functional nodes

--- test/nodes/functional.jl
+++ test/nodes/functional.jl
@@ -21,4 +21,25 @@
 
         @test DiscreteFunctionalNode(name, models, performances, simulation) == DiscreteFunctionalNode(name, models, performances, simulation, parameters)
     end
+
+    @testet "Wrap Model" begin
+        name = :functional
+        model = Model(df -> sqrt.(df.z .^ 2 + df.z .^ 2), :value1)
+        simulation = MonteCarlo(200)
+        performances = df -> 1 .- 2 .* df.value1
+        parameters = Dict{Symbol,Vector{Parameter}}()
+        node = DiscreteFunctionalNode(name, [model], performances, simulation, parameters)
+
+        @test node == DiscreteFunctionalNode(name, model, performances, simulation, parameters)
+        @test node == DiscreteFunctionalNode(name, model, performances, simulation)
+
+        name = :functional
+        model = Model(df -> sqrt.(df.z .^ 2 + df.z .^ 2), :value1)
+        simulation = MonteCarlo(200)
+        discretization = ApproximatedDiscretization()
+        node = ContinuousFunctionalNode(name, [model], simulation, discretization)
+
+        @test node == ContinuousFunctionalNode(name, model, simulation, discretization)
+        @test node == ContinuousFunctionalNode(name, model, simulation)
+    end
 end
