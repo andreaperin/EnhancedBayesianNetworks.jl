115f81c62a08bb5295e4a41d29bddfcfeab79243
Andrea Perin <andrea.perin1992@gmail.com>
2024-11-04 18:48:22 +0100

    Correction for wrap function

--- src/nodes/functional.jl
+++ src/nodes/functional.jl
@@ -3,19 +3,26 @@
     models::Vector{<:UQModel}
     simulation::AbstractMonteCarlo
     discretization::ApproximatedDiscretization
+
+    function ContinuousFunctionalNode(
+        name::Symbol,
+        models::Union{Vector{<:UQModel},<:UQModel},
+        simulation::AbstractMonteCarlo,
+        discretization::ApproximatedDiscretization
+    )
+        models = wrap(models)
+        new(name, models, simulation, discretization)
+    end
 end
 
 function ContinuousFunctionalNode(
     name::Symbol,
-    models::Vector{<:UQModel},
+    models::Union{Vector{<:UQModel},<:UQModel},
     simulation::AbstractMonteCarlo
 )
     discretization = ApproximatedDiscretization()
     ContinuousFunctionalNode(name, models, simulation, discretization)
 end
-
-ContinuousFunctionalNode(name::Symbol, models::UQModel, simulation::AbstractMonteCarlo) = ContinuousFunctionalNode(name, wrap(models), simulation)
-ContinuousFunctionalNode(name::Symbol, models::UQModel, simulation::AbstractMonteCarlo, discretization::ApproximatedDiscretization) = ContinuousFunctionalNode(name, wrap(models), simulation, discretization)
 
 @auto_hash_equals struct DiscreteFunctionalNode <: DiscreteNode
     name::Symbol
@@ -23,20 +30,27 @@ ContinuousFunctionalNode(name::Symbol, models::UQModel, simulation::AbstractMont
     performance::Function
     simulation::Union{AbstractSimulation,DoubleLoop,RandomSlicing}
     parameters::Dict{Symbol,Vector{Parameter}}
+
+    function DiscreteFunctionalNode(
+        name::Symbol,
+        models::Union{Vector{<:UQModel},<:UQModel},
+        performance::Function,
+        simulation::Union{AbstractSimulation,DoubleLoop,RandomSlicing},
+        parameters::Dict{Symbol,Vector{Parameter}}
+    )
+        models = wrap(models)
+        new(name, models, performance, simulation, parameters)
+    end
 end
 
 function DiscreteFunctionalNode(
     name::Symbol,
-    models::Vector{<:UQModel},
+    models::Union{Vector{<:UQModel},<:UQModel},
     performance::Function,
     simulation::Union{AbstractSimulation,DoubleLoop,RandomSlicing}
 )
     parameters = Dict{Symbol,Vector{Parameter}}()
     DiscreteFunctionalNode(name, models, performance, simulation, parameters)
 end
-
-DiscreteFunctionalNode(name::Symbol, models::UQModel, performance::Function, simulation::AbstractMonteCarlo) = DiscreteFunctionalNode(name, wrap(models), performance, simulation)
-DiscreteFunctionalNode(name::Symbol, models::UQModel, performance::Function, simulation::AbstractMonteCarlo, parameters::Dict{Symbol,Vector{Parameter}}) = DiscreteFunctionalNode(name, wrap(models), performance, simulation, parameters)
-
 
 const global FunctionalNode = Union{DiscreteFunctionalNode,ContinuousFunctionalNode}
