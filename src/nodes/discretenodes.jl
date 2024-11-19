7e8f90552c278c739f392b50dc05977c8a8706aa
Andrea Perin <andrea.perin1992@gmail.com>
2024-11-19 11:35:03 +0100

    Add _extreme_points functions for discrete nodes (root and child)

--- src/nodes/discretenodes.jl
+++ src/nodes/discretenodes.jl
@@ -34,12 +34,7 @@ end
 
 function _verify_cpt_and_normalize!(cpt::DataFrame, name::Symbol)
     _verify_cpt_coherence(cpt)
-    if ncol(cpt) == 2     ## Root Nodes
-        sub_cpts = [cpt]
-    else    ## Child Nodes
-        scenarios = unique!(map(s -> _by_row(s), _scenarios(cpt, name)))
-        sub_cpts = map(e -> subset(cpt, e), scenarios)
-    end
+    sub_cpts = _scenarios_cpt(cpt, name)
     _verify_precise_probabilities_values(cpt)
     _verify_imprecise_probabilities_values(cpt)
     map(sc -> _verify_imprecise_exhaustiveness(sc), sub_cpts)
@@ -67,18 +62,37 @@ end
 
 _scenarios(node::DiscreteNode) = _scenarios(node.cpt, node.name)
 
-function _cpt(x::Dict{AbstractVector{Symbol},<:AbstractDiscreteProbability}, indices::AbstractVector{Symbol})
+function _scenarios_cpt(cpt::DataFrame, name::Symbol)
+    if ncol(cpt) == 2     ## Root Nodes
+        sub_cpts = [cpt]
+    else    ## Child Nodes
+        scenarios = unique!(map(s -> _by_row(s), _scenarios(cpt, name)))
+        sub_cpts = map(e -> subset(cpt, e), scenarios)
+    end
+    return sub_cpts
+end
+
+_scenarios_cpt(node::DiscreteNode) = _scenarios_cpt(node.cpt, node.name)
+
+function _cpt(x::Dict{Vector{Symbol},<:AbstractDiscreteProbability}, names::AbstractVector{Symbol})
     cpt = DataFrame()
-    for (i, name) in enumerate(indices)
+    for (i, name) in enumerate(names)
         cpt[!, name] = map(x -> x[i], collect(keys(x)))
     end
     cpt[!, :Prob] = collect(values(x))
     return cpt
 end
 
+function _cpt(x::Dict{Symbol,<:AbstractDiscreteProbability}, name::Symbol)
+    cpt = DataFrame()
+    cpt[!, name] = collect(keys(x))
+    cpt[!, :Prob] = collect(values(x))
+    return cpt
+end
+
 _cpt(x::DataFrame) = x
 
-function _parameters_with_evidence(node::DiscreteNode; evidence::Evidence)
+function _parameters_with_evidence(node::DiscreteNode, evidence::Evidence)
     if node.name ∉ keys(evidence)
         error("evidence $evidence does not contain the node $(node.name)")
     else
@@ -94,29 +108,44 @@ function _extreme_points(node::DiscreteNode)
     if _is_precise(node)
         return [node]
     else
-        ext_points = _extreme_points_probabilities(node.cpt)
-        function f(ext_point)
-            df = node.cpt[!, Not(:Prob)]
-            df[!, :Prob] = ext_point
-            return df
-        end
-        dfs = f.(ext_points)
-        return map(df -> DiscreteNode(node.name, df, node.parameters, node.additional_info), dfs)
+        sub_cpts = _scenarios_cpt(node.cpt, node.name)
+        dfs = map(sc -> _extreme_points_dfs(sc), sub_cpts)
+        dfsa = vec(collect(Iterators.product(dfs...)))
+        res = map(df -> vcat(df...), dfsa)
+        return map(r -> DiscreteNode(node.name, r, node.parameters, node.additional_info), res)
     end
 end
 
-function _extreme_points_probabilities(cpt::DataFrame)
-    n = nrow(cpt)
-    A = zeros(2 * n, n)
-    A[collect(1:2:2*n), :] = Matrix(-1.0I, n, n)
-    A[collect(2:2:2*n), :] = Matrix(1.0I, n, n)
-    A = vcat(A, [-ones(n)'; ones(n)'])
+function _extreme_points_dfs(sub_cpt::DataFrame)
+    ext_points = _extreme_points_probabilities(sub_cpt)
+    function _replace_prob(sub_cpt, ext_points)
+        res = []
+        for e in ext_points
+            df = deepcopy(sub_cpt)
+            df[!, :Prob] = e
+            push!(res, df)
+        end
+        return res
+    end
+    return _replace_prob(sub_cpt, ext_points)
+end
 
-    b = collect(Iterators.flatten(cpt[!, :Prob]))
-    b[collect(1:2:2*n)] = -b[collect(1:2:2*n)]
-    b = vcat(b, [-1 1]')
+function _extreme_points_probabilities(sub_cpt::DataFrame)
+    if all(isa.(sub_cpt[!, :Prob], Vector))
+        n = nrow(sub_cpt)
+        A = zeros(2 * n, n)
+        A[collect(1:2:2*n), :] = Matrix(-1.0I, n, n)
+        A[collect(2:2:2*n), :] = Matrix(1.0I, n, n)
+        A = vcat(A, [-ones(n)'; ones(n)'])
 
-    h = mapreduce((Ai, bi) -> HalfSpace(Ai, bi), ∩, [A[i, :] for i in axes(A, 1)], b)
-    v = doubledescription(h)
+        b = collect(Iterators.flatten(sub_cpt[!, :Prob]))
+        b[collect(1:2:2*n)] = -b[collect(1:2:2*n)]
+        b = vcat(b, [-1 1]')
+
+        h = mapreduce((Ai, bi) -> HalfSpace(Ai, bi), ∩, [A[i, :] for i in axes(A, 1)], b)
+        v = doubledescription(h)
+    else
+        error("Precise conditional probability table does not have extreme points: $sub_cpt")
+    end
     return v.points.points
 end
