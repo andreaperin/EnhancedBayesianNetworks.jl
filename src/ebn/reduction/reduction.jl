4629dfeaab913bf4cf8f4343e58433c4e519606e
Andrea Perin <andrea.perin1992@gmail.com>
2024-12-02 17:42:50 +0100

    add reduction and related test

--- src/ebn/reduction/reduction.jl
+++ src/ebn/reduction/reduction.jl
@@ -1,28 +1,24 @@
 
 function reduce!(net::EnhancedBayesianNetwork)
-    if isempty(filter(x -> isa(x, FunctionalNode), net.nodes))
-        cont_nodes = filter(x -> isa(x, ContinuousNode), net.nodes)
-        map(x -> _eliminate_continuous_node!(net, x), cont_nodes)
-    else
+    if !isempty(filter(x -> isa(x, FunctionalNode), net.nodes))
         evaluate!(net)
-        cont_nodes = filter(x -> isa(x, ContinuousNode), net.nodes)
-        map(x -> _eliminate_continuous_node!(net, x), cont_nodes)
     end
+    cont_nodes = filter(x -> isa(x, ContinuousNode), net.nodes)
+    map(x -> _eliminate_continuous_node!(net, x), cont_nodes)
     return nothing
 end
 
-
-function dispatch_network(net::EnhancedBayesianNetwork)
-    if isempty(filter(x -> isa(x, ContinuousNode), net.nodes))
-        if all(.!_is_imprecise.(net.nodes))
-            return BayesianNetwork(net.nodes, net.topology_dict, net.adj_matrix)
-        else
-            return CredalNetwork(net.nodes, net.topology_dict, net.adj_matrix)
-        end
-    else
-        return net
-    end
-end
+# function dispatch_network(net::EnhancedBayesianNetwork)
+#     if isempty(filter(x -> isa(x, ContinuousNode), net.nodes))
+#         if all(.!_is_imprecise.(net.nodes))
+#             return BayesianNetwork(net.nodes, net.topology_dict, net.adj_matrix)
+#         else
+#             return CredalNetwork(net.nodes, net.topology_dict, net.adj_matrix)
+#         end
+#     else
+#         return net
+#     end
+# end
 
 function _eliminate_continuous_node!(net::EnhancedBayesianNetwork, node::ContinuousNode)
     parents_indices = parents(net, node)[1]
