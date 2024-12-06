48cd0695e28460abaabe26d40b2102db172afb4e
Andrea Perin <andrea.perin1992@gmail.com>
2024-12-06 11:11:44 +0100

    remove ConditionalProbabilityDistribution structure

--- test/networks/networks.jl
+++ test/networks/networks.jl
@@ -1,15 +1,15 @@
-@testset "CPD" begin
-    target = :A
-    parents = [:B, :C]
-    parental_ncategories = [2, 2]
-    states = [:a1, :a2]
-    distribution = DataFrame(:B => [:b1, :b1, :b1, :b1, :b2, :b2, :b2, :b2], :C => [:c1, :c1, :c2, :c2, :c1, :c1, :c2, :c2], :Prob => [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5])
+# @testset "CPD" begin
+#     target = :A
+#     parents = [:B, :C]
+#     parental_ncategories = [2, 2]
+#     states = [:a1, :a2]
+#     distribution = DataFrame(:B => [:b1, :b1, :b1, :b1, :b2, :b2, :b2, :b2], :C => [:c1, :c1, :c2, :c2, :c1, :c1, :c2, :c2], :Prob => [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5])
 
-    cpd = ConditionalProbabilityDistribution(target, parents, parental_ncategories, states, distribution)
+#     cpd = ConditionalProbabilityDistribution(target, parents, parental_ncategories, states, distribution)
 
-    @test cpd.target == target
-    @test cpd.parents == parents
-    @test cpd.parental_ncategories == parental_ncategories
-    @test cpd.states == states
-    @test cpd.probabilities == distribution
-end
+#     @test cpd.target == target
+#     @test cpd.parents == parents
+#     @test cpd.parental_ncategories == parental_ncategories
+#     @test cpd.states == states
+#     @test cpd.probabilities == distribution
+# end
