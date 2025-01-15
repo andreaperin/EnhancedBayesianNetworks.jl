34cda72c2f24ee5b6a045728b8066222a913d409
Andrea Perin <andrea.perin1992@gmail.com>
2025-01-15 16:09:52 +0100

    Add dispatch function and relative test

---
+++ src/networks/dispatch.jl
@@ -0,0 +1,18 @@
+function dispatch(net::EnhancedBayesianNetwork)
+    ## If any ContinuousNode is present -> evaluate -> reduce
+    if !isempty(filter(x -> isa(x, ContinuousNode), net.nodes))
+        reduce!(net)
+    end
+    if isempty(filter(x -> isa(x, ContinuousNode), net.nodes))
+        if all(EnhancedBayesianNetworks._is_precise.(net.nodes))
+            return BayesianNetwork(net.nodes, net.topology_dict, net.adj_matrix)
+        else
+            return CredalNetwork(net.nodes, net.topology_dict, net.adj_matrix)
+        end
+    else
+        return net
+    end
+end
+
+dispatch(net::BayesianNetwork) = net
+dispatch(net::CredalNetwork) = net
