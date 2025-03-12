b6f34bbd905cadb989ddda7241a3a6227c417e94
Andrea Perin <andrea.perin1992@gmail.com>
2025-03-12 12:27:52 +0100

    renamed to verify_evidence.jl

--- src/inference/inference.jl
+++ src/inference/inference.jl
@@ -1,6 +1,6 @@
 abstract type AbstractInferenceState end
 
-include("../util/evidence_verification.jl")
+include("../util/verify_evidence.jl")
 include("inferencestate.jl")
 include("factors.jl")
 include("variableselimination.jl")
--- src/util/verify_evidence.jl
+++ src/util/verify_evidence.jl
@@ -1,9 +1,9 @@
-function _are_consistent(a::Evidence, b::Evidence)
-    for key in keys(a)
-        haskey(b, key) && b[key] != a[key] && error("not consistent evidences: $a; $b")
-    end
-    return true
-end
+# function _are_consistent(a::Evidence, b::Evidence)
+#     for key in keys(a)
+#         haskey(b, key) && b[key] != a[key] && error("not consistent evidences: $a; $b")
+#     end
+#     return true
+# end
 
 function _verify_evidence(a::Evidence, bn::BayesianNetwork)
     any(keys(a) .∉ [[i.name for i in bn.nodes]]) && error("all nodes in the $a have to be in the network")
