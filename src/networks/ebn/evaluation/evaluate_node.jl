370cc69b04016b483224bf638b4fda79b1923d16
Andrea Perin <andrea.perin1992@gmail.com>
2025-03-13 17:09:09 +0100

    Update evaluate_node.jl and test

--- src/networks/ebn/evaluation/evaluate_node.jl
+++ src/networks/ebn/evaluation/evaluate_node.jl
@@ -1,10 +1,10 @@
 function _evaluate_node(net::EnhancedBayesianNetwork, node::ContinuousFunctionalNode)
-    if all(_is_precise.(parents(net, node)[3]))
+    if all(isprecise.(parents(net, node)[3]))
         discrete_parents = filter(x -> isa(x, DiscreteNode), parents(net, node)[3])
         continuous_parents = filter(x -> isa(x, ContinuousNode), parents(net, node)[3])
         ancestors = discrete_ancestors(net, node)
 
-        ancestors_combination = sort(vec(collect(Iterators.product(_states.(ancestors)...))))
+        ancestors_combination = sort(vec(collect(Iterators.product(states.(ancestors)...))))
         if isempty(ancestors_combination[1])
             new_cpt = DataFrame()
             evidences = [Evidence()]
@@ -18,8 +18,8 @@ function _evaluate_node(net::EnhancedBayesianNetwork, node::ContinuousFunctional
         dists = []
         add_info = Dict{Vector{Symbol},Dict}()
         for evidence in evidences
-            parameters = mapreduce(p -> _parameters_with_evidence(p, evidence), vcat, discrete_parents; init=UQInput[])
-            randomvariables = mapreduce(p -> _continuous_input(p, evidence), vcat, continuous_parents; init=UQInput[])
+            parameters = mapreduce(p -> _uq_inputs(p, evidence), vcat, discrete_parents; init=UQInput[])
+            randomvariables = mapreduce(p -> _uq_inputs(p, evidence), vcat, continuous_parents; init=UQInput[])
             df = UncertaintyQuantification.sample([parameters..., randomvariables...], node.simulation)
             UncertaintyQuantification.evaluate!(node.models, df)
             pdf = EmpiricalDistribution(df[:, node.models[end].name])
@@ -28,8 +28,8 @@ function _evaluate_node(net::EnhancedBayesianNetwork, node::ContinuousFunctional
         end
 
         new_cpt[!, :Π] = dists
-
-        return ContinuousNode{UnivariateDistribution}(node.name, new_cpt, discretization, add_info)
+        cpt = ContinuousConditionalProbabilityTable{PreciseContinuousInput}(new_cpt)
+        return ContinuousNode(node.name, cpt, discretization, add_info)
     else
         error("node $(node.name) is a continuousfunctionalnode with at least one parent with Interval or p-boxes in its distributions. No method for extracting failure probability p-box have been implemented yet")
     end
@@ -40,7 +40,7 @@ function _evaluate_node(net::EnhancedBayesianNetwork, node::DiscreteFunctionalNo
     continuous_parents = filter(x -> isa(x, ContinuousNode), parents(net, node)[3])
     ancestors = discrete_ancestors(net, node)
 
-    ancestors_combination = sort(vec(collect(Iterators.product(_states.(ancestors)...))))
+    ancestors_combination = sort(vec(collect(Iterators.product(states.(ancestors)...))))
     node_states = [Symbol(string(node.name) * "_fail"), Symbol(string(node.name) * "_safe")]
 
     if isempty(ancestors_combination[1])
@@ -58,8 +58,8 @@ function _evaluate_node(net::EnhancedBayesianNetwork, node::DiscreteFunctionalNo
     additional_info = Dict{AbstractVector{Symbol},Dict}()
     probs = []
     for evidence in evidences
-        parameters = mapreduce(p -> _parameters_with_evidence(p, evidence), vcat, discrete_parents; init=UQInput[])
-        randomvariables = mapreduce(p -> _continuous_input(p, evidence), vcat, continuous_parents; init=UQInput[])
+        parameters = mapreduce(p -> EnhancedBayesianNetworks._uq_inputs(p, evidence), vcat, discrete_parents; init=UQInput[])
+        randomvariables = mapreduce(p -> EnhancedBayesianNetworks._uq_inputs(p, evidence), vcat, continuous_parents; init=UQInput[])
         res = probability_of_failure(node.models, node.performance, [parameters..., randomvariables...], node.simulation)
 
         if isa(node.simulation, Union{AbstractMonteCarlo,LineSampling,ImportanceSampling,UncertaintyQuantification.AbstractSubSetSimulation})
@@ -68,13 +68,13 @@ function _evaluate_node(net::EnhancedBayesianNetwork, node::DiscreteFunctionalNo
             additional_info[collect(values(evidence))] = Dict(:cov => res[2], :samples => res[3])
         elseif isa(node.simulation, DoubleLoop)
             !isa(res, Interval) ? res = Interval(res, res, :pf) : nothing
-            push!(probs, [res.lb, res.ub])
-            push!(probs, [1 - res.ub, 1 - res.lb])
+            push!(probs, (res.lb, res.ub))
+            push!(probs, (1 - res.ub, 1 - res.lb))
             additional_info[collect(values(evidence))] = Dict()
         elseif isa(node.simulation, RandomSlicing)
             !isa(res[1], Interval) ? res[1] = Interval(res[1], res[1], :pf) : nothing
-            push!(probs, [res[1].lb, res[1].ub])
-            push!(probs, [1 - res[1].ub, 1 - res[1].lb])
+            push!(probs, (res[1].lb, res[1].ub))
+            push!(probs, (1 - res[1].ub, 1 - res[1].lb))
             additional_info[collect(values(evidence))] = Dict(:lb => res[2], :ub => res[3])
         elseif isa(node.simulation, FORM)
             push!(probs, res[1])
@@ -82,8 +82,13 @@ function _evaluate_node(net::EnhancedBayesianNetwork, node::DiscreteFunctionalNo
             additional_info[collect(values(evidence))] = Dict(:β => res[2], :design_point => res[3], :α => res[4])
         end
     end
-
     new_cpt[!, :Π] = probs
 
-    return DiscreteNode(node.name, new_cpt, node.parameters, additional_info)
+    if all(isa.(probs, Real))
+        cpt = DiscreteConditionalProbabilityTable{PreciseDiscreteProbability}(new_cpt)
+    else
+        cpt = DiscreteConditionalProbabilityTable{ImpreciseDiscreteProbability}(new_cpt)
+    end
+
+    return DiscreteNode(node.name, cpt, node.parameters, additional_info)
 end
