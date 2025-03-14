dd1d3e39b2077ec4ae3228e7701ac609fa4f3975
Andrea Perin <andrea.perin1992@gmail.com>
2025-03-14 16:58:16 +0100

    Update verify_inference.jl and inference_state.jl and test

--- test/inference/inferencestate.jl
+++ test/inference/inferencestate.jl
@@ -1,8 +1,29 @@
 @testset "Inference State" begin
-    v = DiscreteNode(:V, DataFrame(:V => [:yesV, :noV], :Π => [0.01, 0.99]))
-    s = DiscreteNode(:S, DataFrame(:S => [:yesS, :noS], :Π => [0.5, 0.5]))
-    t = DiscreteNode(:T, DataFrame(:V => [:yesV, :yesV, :noV, :noV], :T => [:yesT, :noT, :yesT, :noT], :Π => [0.05, 0.95, 0.01, 0.99]))
-    l = DiscreteNode(:L, DataFrame(:S => [:yesS, :yesS, :noS, :noS], :L => [:yesL, :noL, :yesL, :noL], :Π => [0.1, 0.9, 0.01, 0.99]))
+
+    cpt_v = DiscreteConditionalProbabilityTable{PreciseDiscreteProbability}(:V)
+    cpt_v[:V=>:yesV] = 0.01
+    cpt_v[:V=>:noV] = 0.99
+    v = DiscreteNode(:V, cpt_v)
+
+    cpt_s = DiscreteConditionalProbabilityTable{PreciseDiscreteProbability}(:S)
+    cpt_s[:S=>:yesS] = 0.5
+    cpt_s[:S=>:noS] = 0.5
+    s = DiscreteNode(:S, cpt_s)
+
+    cpt_t = DiscreteConditionalProbabilityTable{PreciseDiscreteProbability}([:V, :T])
+    cpt_t[:V=>:yesV, :T=>:yesT] = 0.05
+    cpt_t[:V=>:yesV, :T=>:noT] = 0.95
+    cpt_t[:V=>:noV, :T=>:yesT] = 0.01
+    cpt_t[:V=>:noV, :T=>:noT] = 0.99
+    t = DiscreteNode(:T, cpt_t)
+
+    cpt_l = DiscreteConditionalProbabilityTable{PreciseDiscreteProbability}([:S, :L])
+    cpt_l[:S=>:yesS, :L=>:yesL] = 0.1
+    cpt_l[:S=>:yesS, :L=>:noL] = 0.9
+    cpt_l[:S=>:noS, :L=>:yesL] = 0.01
+    cpt_l[:S=>:noS, :L=>:noL] = 0.99
+    l = DiscreteNode(:L, cpt_l)
+
     bn = BayesianNetwork([v, s, t, l])
     add_child!(bn, v, t)
     add_child!(bn, s, l)
@@ -20,11 +41,42 @@
     @test PreciseInferenceState(bn, :V, Evidence()).query == PreciseInferenceState(bn, [:V], Evidence()).query
     @test PreciseInferenceState(bn, :V, Evidence()).evidence == PreciseInferenceState(bn, [:V], Evidence()).evidence
 
-    F = DiscreteNode(:F, DataFrame(:F => [:Ft, :Ff], :Π => [[0.4, 0.5], [0.5, 0.6]]))
-    B = DiscreteNode(:B, DataFrame(:B => [:Bt, :Bf], :Π => [0.5, 0.5]))
-    L = DiscreteNode(:L, DataFrame(:F => [:Ft, :Ft, :Ft, :Ff, :Ff, :Ff], :L => [:Lt, :Lf, :L2, :Lt, :Lf, :L2], :Π => [0.3, 0.4, 0.3, 0.05, 0.85, 0.1]))
-    D = DiscreteNode(:D, DataFrame(:F => [:Ft, :Ft, :Ft, :Ft, :Ff, :Ff, :Ff, :Ff], :B => [:Bt, :Bt, :Bf, :Bf, :Bt, :Bt, :Bf, :Bf], :D => [:Dt, :Df, :Dt, :Df, :Dt, :Df, :Dt, :Df], :Π => [0.8, 0.2, 0.1, 0.9, 0.1, 0.9, 0.7, 0.3]))
-    H = DiscreteNode(:H, DataFrame(:D => [:Dt, :Dt, :Df, :Df], :H => [:Ht, :Hf, :Ht, :Hf], :Π => [0.6, 0.4, 0.3, 0.7]))
+    cpt_f = DiscreteConditionalProbabilityTable{ImpreciseDiscreteProbability}(:F)
+    cpt_f[:F=>:Ft] = (0.4, 0.5)
+    cpt_f[:F=>:Ff] = (0.5, 0.6)
+    F = DiscreteNode(:F, cpt_f)
+
+    cpt_b = DiscreteConditionalProbabilityTable{PreciseDiscreteProbability}(:B)
+    cpt_b[:B=>:Bt] = 0.5
+    cpt_b[:B=>:Bf] = 0.5
+    B = DiscreteNode(:B, cpt_b)
+
+    cpt_l = DiscreteConditionalProbabilityTable{PreciseDiscreteProbability}([:F, :L])
+    cpt_l[:F=>:Ft, :L=>:Lt] = 0.3
+    cpt_l[:F=>:Ft, :L=>:Lf] = 0.4
+    cpt_l[:F=>:Ft, :L=>:L2] = 0.3
+    cpt_l[:F=>:Ff, :L=>:Lt] = 0.05
+    cpt_l[:F=>:Ff, :L=>:Lf] = 0.85
+    cpt_l[:F=>:Ff, :L=>:L2] = 0.1
+    L = DiscreteNode(:L, cpt_l)
+
+    cpt_d = DiscreteConditionalProbabilityTable{PreciseDiscreteProbability}([:F, :B, :D])
+    cpt_d[:F=>:Ft, :B=>:Bt, :D=>:Dt] = 0.8
+    cpt_d[:F=>:Ft, :B=>:Bt, :D=>:Df] = 0.2
+    cpt_d[:F=>:Ft, :B=>:Bf, :D=>:Dt] = 0.1
+    cpt_d[:F=>:Ft, :B=>:Bf, :D=>:Df] = 0.9
+    cpt_d[:F=>:Ff, :B=>:Bt, :D=>:Dt] = 0.1
+    cpt_d[:F=>:Ff, :B=>:Bt, :D=>:Df] = 0.9
+    cpt_d[:F=>:Ff, :B=>:Bf, :D=>:Dt] = 0.7
+    cpt_d[:F=>:Ff, :B=>:Bf, :D=>:Df] = 0.3
+    D = DiscreteNode(:D, cpt_d)
+
+    cpt_h = DiscreteConditionalProbabilityTable{PreciseDiscreteProbability}([:D, :H])
+    cpt_h[:D=>:Dt, :H=>:Ht] = 0.6
+    cpt_h[:D=>:Dt, :H=>:Hf] = 0.4
+    cpt_h[:D=>:Df, :H=>:Ht] = 0.3
+    cpt_h[:D=>:Df, :H=>:Hf] = 0.7
+    H = DiscreteNode(:H, cpt_h)
 
     cn = CredalNetwork([F, B, L, D, H])
 
