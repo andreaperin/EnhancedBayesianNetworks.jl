140a7dae898224112e44479caff06d1d1880d659
Andrea Perin <andrea.perin1992@gmail.com>
2025-03-14 16:17:35 +0100

    Update dispatch.jl and test

--- src/networks/dispatch.jl
+++ src/networks/dispatch.jl
@@ -4,7 +4,7 @@ function dispatch(net::EnhancedBayesianNetwork)
         reduce!(net)
     end
     if isempty(filter(x -> isa(x, ContinuousNode), net.nodes))
-        if all(EnhancedBayesianNetworks._is_precise.(net.nodes))
+        if all(isprecise.(net.nodes))
             return BayesianNetwork(net.nodes, net.topology_dict, net.adj_matrix)
         else
             return CredalNetwork(net.nodes, net.topology_dict, net.adj_matrix)
