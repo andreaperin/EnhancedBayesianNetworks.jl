f687ffd43e893bde9ea10be6027b2a8a67865079
Documenter.jl <documenter@juliadocs.github.io>
2026-09-12 20:34:11 +0000

    delete history

---
+++ previews/PR53/assets/chunks/logo.Bnss7MU1.js
@@ -0,0 +1,1 @@
+const s="/EnhancedBayesianNetworks.jl/previews/PR53/assets/logo.B7NaPgM9.png";export{s as _};
---
+++ versions.js
@@ -0,0 +1,7 @@
+var DOC_VERSIONS = [
+  "dev",
+  "stable",
+  "v0.2",
+];
+var DOCUMENTER_NEWEST = "0.2.0";
+var DOCUMENTER_STABLE = "stable";
